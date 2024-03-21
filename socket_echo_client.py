d8793e32e46e208e05c5e16493add69b837d9a69
GC Neal Christian S. Clarin <gcnealchristian.clarin@g.msuiit.edu.ph>
2024-03-21 21:28:28 +0800

    Refactor code to improve performance and readability

---
+++ socket_echo_client.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 14344)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+try:
+    while True:  # Keep asking for user input
+        # Send data
+        message = input("Enter a message (or 'exit' to quit): ")  # Get user input
+        if message == 'exit':
+            break  # Exit the loop if the user types 'exit'
+        print('sending: ', message)
+        sock.sendall(message.encode())
+        
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+        
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print('received: ', data.decode())
+finally:
+    print('closing socket')
+    sock.close()
